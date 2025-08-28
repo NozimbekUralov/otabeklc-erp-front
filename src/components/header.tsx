266f819932c7e8a26c90d65cc6b7da7a9a36a4e2
NozimbekUralov <nozimurolov1@gmail.com>
2025-08-28 21:05:08 +0500

    update header to use new logo component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,13 @@
 'use client'
 
-import Image from "next/image"
 import { Button } from "./ui/button"
 import Link from "next/link"
+import { Logo } from "@/components/logo"
 
 export function Header() {
   return (
-    <header className="sticky top-0 z-50 w-full bg-accent flex justify-between items-center p-4">
-      <Link href={'/'} className="flex gap-2 items-center">
-        <Image src="/logo.png" alt="logo" width={40} height={40} />
-        <h1 className="text-3xl font-bold">Otabek L.C</h1>
-      </Link>
+    <header className="sticky top-0 z-50 w-full flex justify-between items-center p-4">
+      <Logo />
       <Link href={'/auth/login'}>
         <Button variant={'outline'}>Sign in</Button>
       </Link>
