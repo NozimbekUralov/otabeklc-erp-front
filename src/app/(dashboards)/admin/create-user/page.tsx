fe996dd1f1679bcd954e4dc6af9d26ff5ce81dff
NozimbekUralov <nozimurolov1@gmail.com>
2025-09-05 09:07:42 +0500

    delete redundant code

--- src/app/(dashboards)/admin/create-user/page.tsx
+++
@@ -1,9 +0,0 @@
-import { CreateUserForm } from '@/components/forms/create-user-form'
-
-export default function CreateUserPage() {
-  return (
-    <div className="w-full max-w-md mx-auto">
-      <CreateUserForm />
-    </div>
-  )
-}
